24436fc7ee63b0d3967217968b7fe27099d4190c
KJ <xiankai@gmail.com>
2013-12-28 18:42:03 +0800

    Initial commit

---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$mandrill = new Mandrill('n4K2QMp31EgJx0umLxX4Ig');
+
+if (!empty($_POST['mandrill_events'])) {	
+	$payload = json_decode($_POST['mandrill_events'], true);
+	if (isset($payload['msg']['raw_msg'])) {
+		$raw_message = $payload['msg']['raw_msg'];
+
+		$params = array(
+			'raw_message' => $message['raw_msg'],
+		);
+
+		$mandrill->call('messages/send-raw.json', $params);
+	}
+}
