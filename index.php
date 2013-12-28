222df11f55bd2c2839746bba12860253772b8268
KJ <xiankai@gmail.com>
2013-12-28 19:16:08 +0800

    Mandrill sends a bunch of messages, not one

--- index.php
+++ index.php
@@ -6,13 +6,21 @@ $mandrill = new Mandrill('n4K2QMp31EgJx0umLxX4Ig');
 
 if (!empty($_POST['mandrill_events'])) {	
 	$payload = json_decode($_POST['mandrill_events'], true);
-	if (isset($payload['msg']['raw_msg'])) {
-		$raw_message = $payload['msg']['raw_msg'];
 
-		$params = array(
-			'raw_message' => $message['raw_msg'],
-		);
-
-		$mandrill->call('messages/send-raw.json', $params);
+	if (!is_array($payload)) {
+		exit;
 	}
+
+	foreach ($payload as $message) {
+		if (isset($payload['msg']['raw_msg'])) {
+			$raw_message = $payload['msg']['raw_msg'];
+
+			$params = array(
+				'raw_message' => $message['raw_msg'],
+			);
+
+			$mandrill->call('messages/send-raw.json', $params);
+		}	
+	}
+	
 }
